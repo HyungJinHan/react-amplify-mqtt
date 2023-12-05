a9907b15b02ac598d8f08c5e6cd01adaae707d19
HyungJinHan <han1210_36@naver.com>
2023-12-05 16:50:10 +0900

    기본 cognito, amplify 설정 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { PubSub } from "@aws-amplify/pubsub";
+import { Authenticator, withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from "aws-amplify";
+import { useEffect, useState } from "react";
+import "./App.css";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  const pubsub = new PubSub({
+    region: "ap-northeast-2",
+    endpoint:
+      // "mqtts://a3jmtb9lvgjr1c-ats.iot.ap-northeast-2.amazonaws.com:8883",
+      "wss://a3jmtb9lvgjr1c-ats.iot.ap-northeast-2.amazonaws.com/mqtt",
+  });
+
+  console.log(pubsub);
+
+  // fetchAuthSession().then((info) => {
+  //   const cognitoIdentityId = info.identityId;
+  //   console.log(cognitoIdentityId);
+  // });
+
+  useEffect(() => {
+    pubsub.subscribe({ topics: "odn/+/sensors" }).subscribe({
+      next: (data) => {
+        console.log(data);
+        setMessage(data.msg);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <div className="App-header">
+            <p>
+              Welcome <code>{user.username}</code>
+            </p>
+
+            <p>
+              email : <code>{user.signInDetails.loginId}</code>
+            </p>
+
+            <p>
+              Connecting State : <code>{pubsub.connectionState}</code>
+              {console.log(pubsub.connectionState)}
+            </p>
+
+            <p>
+              MQTT Message : <code>{message}</code>
+            </p>
+
+            <button style={{ cursor: "pointer" }} onClick={signOut}>
+              Sign out
+            </button>
+          </div>
+        )}
+      </Authenticator>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
