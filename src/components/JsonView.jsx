52c762c6efbea1c9d38ed3052218a26da57540b3
HyungJinHan <han1210_36@naver.com>
2023-12-07 16:14:25 +0900

    custom theme 추가

--- src/components/JsonView.jsx
+++ src/components/JsonView.jsx
@@ -1,6 +1,27 @@
 import React from "react";
 import { JSONTree } from "react-json-tree";
 
+const customTheme = {
+  // scheme: 'google',
+  // author: 'seth wright (http://sethawright.com)',
+  base00: "#1d1f21",
+  base01: "#282a2e",
+  base02: "#373b41",
+  base03: "#969896",
+  base04: "#b4b7b4",
+  base05: "#c5c8c6",
+  base06: "#e0e0e0",
+  base07: "#ffffff",
+  base08: "#CC342B",
+  base09: "#F96A38",
+  base0A: "#FBA922",
+  base0B: "#42e3ff",
+  base0C: "#3971ED",
+  base0D: "#276bfe",
+  base0E: "#A36AC7",
+  base0F: "#3971ED",
+};
+
 export default function JsonView({ data, keyPath }) {
   return (
     <JSONTree
@@ -10,7 +31,7 @@ export default function JsonView({ data, keyPath }) {
       valueRenderer={(raw) => <em>{raw}</em>}
       keyPath={[keyPath]}
       theme={{
-        extend: "google",
+        extend: { ...customTheme },
         valueLabel: {
           textDecoration: "underline",
         },
